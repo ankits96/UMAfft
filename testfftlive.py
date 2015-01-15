6eeb58d1933b060ffc67ffd3b67d3d0478b14687
Ankit Shah <ank.h.shah@gmail.com>
2015-01-14 20:25:33 -0500

    Some cleaning up and deletions

--- testfftlive.py
+++ testfftlive.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# 8 bar Audio equaliser using MCP2307
+# This script attempts to do an FFT on sound data in real time. Currently not entirely too accurate
  
 import alsaaudio as aa
 import time
@@ -9,30 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 
-
-#bus=smbus.SMBus(0)     #Use '1' for newer Pi boards;
-
-#ADDR   = 0x20         #The I2C address of MCP23017
-#DIRA   = 0x00         #PortA I/O direction, by pin. 0=output, 1=input
-#DIRB   = 0x01         #PortB I/O direction, by pin. 0=output, 1=input
-#BANKA  = 0x12         #Register address for Bank A
-#BANKB  = 0x13         #Register address for Bank B
-
-#Set up the 23017 for 16 output pins
-#bus.write_byte_data(ADDR, DIRA, 0);  #all zeros = all outputs on Bank A
-#bus.write_byte_data(ADDR, DIRB, 0);  #all zeros = all outputs on Bank B
-
-#def TurnOffLEDS ():
- #  bus.write_byte_data(ADDR, BANKA, 0xFF)  #set all columns high
-  # bus.write_byte_data(ADDR, BANKB, 0x00)  #set all rows low
-
-#def Set_Column(row, col):
- #  TurnOffLEDS()
-  # bus.write_byte_data(ADDR, BANKA, col)
-   #bus.write_byte_data(ADDR, BANKB, row)
-         
-# Initialise matrix
-#TurnOffLEDS()
 
 # Set up audio
 sample_rate = 96000
@@ -44,9 +20,6 @@ data_in.setrate(sample_rate)
 data_in.setformat(aa.PCM_FORMAT_S16_LE)
 data_in.setperiodsize(chunk)
 
-#Set up animation
-#def init()
-
 def calculate_levels(data, chunk,sample_rate):
    # Convert raw data to numpy array
    data = unpack("%dh"%(len(data)/2),data)
@@ -57,12 +30,11 @@ def calculate_levels(data, chunk,sample_rate):
    fourier=np.delete(fourier,len(fourier)-1)
    # Find amplitude
    power =(np.abs(fourier))
-   # Araange array into 8 rows for the 8 bars on LED matrix
-   #power = np.reshape(power,(8,chunk/8))
-   #matrix= np.int_(np.average(power,axis=1)/4)
    return power
 
 print "Processing....."
+
+#most comments are for debugging purposes
 
 matrix2=0
 timeout = time.time()+2
@@ -76,35 +48,30 @@ while True:
       #try:
    matrix=calculate_levels(data, chunk,sample_rate)
    #print matrix.shape
-   matrix[:200] = 0
+   matrix[:200] = 0 #throws out the edges since they dont seem to be necessary
    matrix[1900:]=0
-   matrix2+=matrix
+   matrix2+=matrix #to have aggregate data
    xaxis = scipy.linspace(0,sample_rate/2, len(data)/4)
    #plt.plot(xaxis,matrix)
    #plt.xlim(0,20000)
    #plt.show()
-         #plt.close()
-         #print matrix2.shape
-         #print xaxis.shape
-         #print matrix
-         #for i in range (0,8):
-         #   Set_Column((1<<matrix[i])-1,0xFF^(1<<i))
-      #except audioop.error, e:
-      #   if e.message !="not a whole number of frames":
-      #      raise e
-   time.sleep(.1)
+   #plt.close()
+   #print matrix2.shape
+   #print xaxis.shape
+   #print matrix
+   time.sleep(.1) #wait before capturing
    data_in.pause(0) # Resume capture
    iters += 1
-   if time.time()>timeout:
+   if time.time()>timeout: #runs for 2 seconds
        break
 
 #total = sum(matrix2[1300:1350])
 #matrix2[1300:1350] = 0
 #print total/iters
-print iters
-print sum(matrix2)
+#print iters
+#print sum(matrix2)
 #matrix2 /= iters
-print sum(matrix2)
+#print sum(matrix2)
 plt.plot(xaxis, matrix2)
 #plt.xlim(0,20000)
 plt.ylim(500)
